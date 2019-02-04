f9712b20a23107c521d29542cb39026c3db10a83
Leif Lampater <leiflampater@gmail.com>
2019-02-04 11:02:31 +0100

    After working with spies

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,26 @@
-import { VoteComponent } from './vote.component'; 
+import {VoteComponent} from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+
+  it('should increment totalVotes when upvoted', () => {
+
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downVoted', () => {
+
+    component.downVote();
+
+    expect(component.totalVotes).toBe(-1);
   });
 });
